76f44a641247311de2d9ef64ea6f30fa2b296714
Ryan Twomey <rtwomey@gmail.com>
2012-12-06 23:14:40 -0500

    Add POSTing notification
    * Adds AFNetworking

--- PushLayerTest/ViewController.m
+++ PushLayerTest/ViewController.m
@@ -6,26 +6,24 @@
 //  Copyright (c) 2012 Draconis Software, LLC. All rights reserved.
 //
 
+#import "AFHTTPClient.h"
+
 #import "AppDelegate.h"
 #import "ViewController.h"
 
+#define kAPIBase								@"http://192.168.1.9:5000"
+#define kAPIKey									@"e6cfb022e1b4bbe10865acc8ca76f9d0"
+#define kNotificationEndpoint		@"applications/2/notifications.json"
+
 @interface ViewController ()
+
+- (NSDictionary *)notificationParamsForDeviceToken:(NSString *)deviceToken;
+- (NSString *)notificationPath;
+- (void)sendPushNotification:(NSString *)deviceToken;
 
 @end
 
 @implementation ViewController
-
-- (void)viewDidLoad {
-	[super viewDidLoad];
-
-	// Do any additional setup after loading the view, typically from a nib.
-}
-
-- (void)didReceiveMemoryWarning {
-	[super didReceiveMemoryWarning];
-
-	// Dispose of any resources that can be recreated.
-}
 
 #pragma mark - Callbacks
 
@@ -34,7 +32,7 @@
 	NSString *deviceToken = [appDelegate deviceToken];
 
 	if (deviceToken != nil) {
-		// TODO: communicate with PushLayer to send the push notification
+		[self sendPushNotification:deviceToken];
 	} else {
 		UIAlertView *alert = [[UIAlertView alloc] initWithTitle:@"No Device Token"
 																										message:@"You need to authorize this app before pushing!"
@@ -43,6 +41,30 @@
 																					otherButtonTitles:nil];
 		[alert show];
 	}
+}
+
+#pragma mark - Private methods
+
+- (NSDictionary *)notificationParamsForDeviceToken:(NSString *)deviceToken {
+	return [NSDictionary dictionaryWithObjectsAndKeys:
+					deviceToken, @"notification[device_token]",
+					@"Hello from PushLayer!", @"notification[alert_body]",
+					nil];
+}
+
+- (NSString *)notificationPath {
+	return [NSString stringWithFormat:@"%@?api_key=%@", kNotificationEndpoint, kAPIKey];
+}
+
+- (void)sendPushNotification:(NSString *)deviceToken {
+	AFHTTPClient *client = [[AFHTTPClient alloc] initWithBaseURL:[NSURL URLWithString:kAPIBase]];
+
+	[client postPath:[self notificationPath] parameters:[self notificationParamsForDeviceToken:deviceToken]
+					 success:^(AFHTTPRequestOperation *operation, id responseObject) {
+	  NSLog(@"Successfully queued push notification with PushLayer.");
+	} failure:^(AFHTTPRequestOperation *operation, NSError *error) {
+		NSLog(@"%@", [error localizedDescription]);
+	}];
 }
 
 @end
